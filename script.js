e7a8b6817554520bd39f37497151bb355be9858c
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-03-11 15:16:36 +1100

    push the site

---
+++ script.js
@@ -0,0 +1,210 @@
+const months = {
+  0: "Jan",
+  1: "Feb",
+  2: "Mar",
+  3: "Apr",
+  4: "May",
+  5: "Jun",
+  6: "Jul",
+  7: "Aug",
+  8: "Sep",
+  9: "Oct",
+  10: "Nov",
+  11: "Dec",
+};
+const daysEl = document.querySelector(".days");
+const hoursEl = document.querySelector(".hours");
+const minutesEl = document.querySelector(".minutes");
+const secondsEl = document.querySelector(".seconds");
+const dateEl = document.querySelector(".date");
+
+const currentDate = new Date();
+let launchDate = new Date();
+launchDate.setDate(currentDate.getDate() + 30);
+
+dateEl.textContent = `${launchDate.getDate()} ${
+  months[launchDate.getMonth()]
+} ${launchDate.getFullYear()}`;
+
+const dayInMs = 1000 * 60 * 60 * 24;
+const hourInMs = 1000 * 60 * 60;
+const minuteInMs = 1000 * 60;
+
+const countDownInterval = setInterval(() => {
+  const now = new Date().getTime();
+  const timeToGo = launchDate - now;
+
+  const days = Math.floor(timeToGo / dayInMs);
+  const hours = Math.floor((timeToGo % dayInMs) / hourInMs);
+  const minutes = Math.floor((timeToGo % hourInMs) / minuteInMs);
+  const seconds = Math.floor((timeToGo % minuteInMs) / 1000);
+
+  daysEl.textContent = padZero(days);
+  hoursEl.textContent = padZero(hours);
+  minutesEl.textContent = padZero(minutes);
+  secondsEl.textContent = padZero(seconds);
+
+  if (timeToGo < 0) {
+    clearInterval(countDownInterval);
+  }
+}, 1000);
+
+function padZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+// Custom Select
+const elSelectNative = document.querySelector(".js-selectNative");
+const elSelectCustom = document.querySelector(".js-selectCustom");
+const elSelectCustomBox = elSelectCustom.children[0];
+const elSelectCustomOpts = elSelectCustom.children[1];
+const customOptsList = Array.from(elSelectCustomOpts.children);
+const optionsCount = customOptsList.length;
+const defaultLabel = elSelectCustomBox.getAttribute("data-value");
+
+let optionChecked = "";
+let optionHoveredIndex = -1;
+
+// Toggle custom select visibility when clicking the box
+elSelectCustomBox.addEventListener("click", e => {
+  const isClosed = !elSelectCustom.classList.contains("isActive");
+
+  if (isClosed) {
+    openSelectCustom();
+  } else {
+    closeSelectCustom();
+  }
+});
+
+function openSelectCustom() {
+  elSelectCustom.classList.add("isActive");
+  // Remove aria-hidden in case this was opened by a user
+  // who uses AT (e.g. Screen Reader) and a mouse at the same time.
+  elSelectCustom.setAttribute("aria-hidden", false);
+
+  if (optionChecked) {
+    const optionCheckedIndex = customOptsList.findIndex(
+      el => el.getAttribute("data-value") === optionChecked
+    );
+    updateCustomSelectHovered(optionCheckedIndex);
+  }
+
+  // Add related event listeners
+  document.addEventListener("click", watchClickOutside);
+  document.addEventListener("keydown", supportKeyboardNavigation);
+}
+
+function closeSelectCustom() {
+  elSelectCustom.classList.remove("isActive");
+
+  elSelectCustom.setAttribute("aria-hidden", true);
+
+  updateCustomSelectHovered(-1);
+
+  // Remove related event listeners
+  document.removeEventListener("click", watchClickOutside);
+  document.removeEventListener("keydown", supportKeyboardNavigation);
+}
+
+function updateCustomSelectHovered(newIndex) {
+  const prevOption = elSelectCustomOpts.children[optionHoveredIndex];
+  const option = elSelectCustomOpts.children[newIndex];
+
+  if (prevOption) {
+    prevOption.classList.remove("isHover");
+  }
+  if (option) {
+    option.classList.add("isHover");
+  }
+
+  optionHoveredIndex = newIndex;
+}
+
+function updateCustomSelectChecked(value, text) {
+  const prevValue = optionChecked;
+
+  const elPrevOption = elSelectCustomOpts.querySelector(
+    `[data-value="${prevValue}"`
+  );
+  const elOption = elSelectCustomOpts.querySelector(`[data-value="${value}"`);
+
+  if (elPrevOption) {
+    elPrevOption.classList.remove("isActive");
+  }
+
+  if (elOption) {
+    elOption.classList.add("isActive");
+  }
+
+  elSelectCustomBox.textContent = text;
+  optionChecked = value;
+}
+
+function watchClickOutside(e) {
+  const didClickedOutside = !elSelectCustom.contains(event.target);
+  if (didClickedOutside) {
+    closeSelectCustom();
+  }
+}
+
+function supportKeyboardNavigation(e) {
+  // press down -> go next
+  if (event.keyCode === 40 && optionHoveredIndex < optionsCount - 1) {
+    let index = optionHoveredIndex;
+    e.preventDefault(); // prevent page scrolling
+    updateCustomSelectHovered(optionHoveredIndex + 1);
+  }
+
+  // press up -> go previous
+  if (event.keyCode === 38 && optionHoveredIndex > 0) {
+    e.preventDefault(); // prevent page scrolling
+    updateCustomSelectHovered(optionHoveredIndex - 1);
+  }
+
+  // press Enter or space -> select the option
+  if (event.keyCode === 13 || event.keyCode === 32) {
+    e.preventDefault();
+
+    const option = elSelectCustomOpts.children[optionHoveredIndex];
+    const value = option && option.getAttribute("data-value");
+
+    if (value) {
+      elSelectNative.value = value;
+      updateCustomSelectChecked(value, option.textContent);
+    }
+    closeSelectCustom();
+  }
+
+  // press ESC -> close selectCustom
+  if (event.keyCode === 27) {
+    closeSelectCustom();
+  }
+}
+
+// Update selectCustom value when selectNative is changed.
+elSelectNative.addEventListener("change", e => {
+  const value = e.target.value;
+  const elRespectiveCustomOption = elSelectCustomOpts.querySelectorAll(
+    `[data-value="${value}"]`
+  )[0];
+
+  updateCustomSelectChecked(value, elRespectiveCustomOption.textContent);
+});
+
+// Update selectCustom value when an option is clicked or hovered
+customOptsList.forEach(function (elOption, index) {
+  elOption.addEventListener("click", e => {
+    const value = e.target.getAttribute("data-value");
+
+    // Sync native select to have the same value
+    elSelectNative.value = value;
+    updateCustomSelectChecked(value, e.target.textContent);
+    closeSelectCustom();
+  });
+
+  elOption.addEventListener("mouseenter", e => {
+    updateCustomSelectHovered(index);
+  });
+
+  // TODO: Toggle these event listeners based on selectCustom visibility
+});
